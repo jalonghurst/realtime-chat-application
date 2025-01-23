f816fea06935e086eb4dc49c9fe41ac63ef9407c
unknown <jacqui.longhurst@btinternet.com>
2025-01-23 14:58:05 +0100

    Add tailwind styling to clientside components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,14 @@
-import './App.css'
-import ChatRoom from './components/chat-room'
+import "./App.css";
+import ChatRoom from "./components/chat-room";
 
 function App() {
-
   return (
-    <>
-      <div>
-        <h1>Chat app</h1>
+    <div className="flex flex-col items-center justfy-center w-full h-full max-h-screen p-2 bg-gray-100 lg:p-8">
+      <div className="flex flex-col w-full h-full p-2 bg-white rounded-lg shadow-md lg:p-6">
         <ChatRoom />
-      </div>
-    </>
-  )
+        </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- client/src/components/chat-room.tsx
+++ client/src/components/chat-room.tsx
@@ -4,12 +4,15 @@ import { Message } from "../types/message";
 
 const ChatRoom: React.FC = () => {
   // State to store inputted message
-    const [messageInput, setMessageInput] = React.useState<string>("");
-    const [messages, setMessages] = React.useState<Message[]>(mockMessages);
+  const [messageInput, setMessageInput] = React.useState<string>("");
+  const [messages, setMessages] = React.useState<Message[]>(mockMessages);
 
   return (
-      <div>
-          <div>
+    <div className="flex flex-col h-full w-full overflow-hidden">
+      <div className="">Status Meetup Standup</div>
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <div> Participants(8)</div>
+        <div className="flex-1 p-4 overflow-y-auto bg-white overflow-y-auto">
           {messages.map((msg) => (
             <div key={msg.messageId} className="flex flex-col mb-2 group">
               <div>
@@ -21,14 +24,23 @@ const ChatRoom: React.FC = () => {
               <p>{msg.message}</p>
             </div>
           ))}
-          </div>
-      <input
-        type="text"
-        value={messageInput}
-        onChange={(e) => setMessageInput(e.target.value)}
-        placeholder="Message"
-      />
-      <button onClick={() => console.log("send message")}>Send</button>
+        </div>
+      </div>
+      <div className="flex p-2 border-t border-gray-300 flex-shrink-0 bordert-t-1 bg-white">
+        <input
+          type="text"
+          value={messageInput}
+          onChange={(e) => setMessageInput(e.target.value)}
+          placeholder="Message"
+          className="flex-grow p-2 border border-gray-300 rounded-l"
+        />
+        <button
+          onClick={() => console.log("send message")}
+          className="p-2 text-white bg-blue-500 rounded-l hover:bg-gray-200"
+        >
+          Send
+        </button>
+      </div>
     </div>
   );
 };
