5761c2688e5b062dfe9fac7ba095aac44a484657
unknown <jacqui.longhurst@btinternet.com>
2025-01-23 16:12:11 +0100

    Create event listener for message creation and broadcasting on serverside

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ const server = http.createServer(app);
 // Create a socket.io server and attach it to the http server
 const io = socketIo(server);
 
+let messages = [];
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -17,6 +19,21 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   const { username } = socket.handshake.query;
   console.log(`New client connected: ${socket.id} with username ${username}`);
+});
+
+// Listen for new messages, and broadcast them to all connected clients
+socket.on("message", (messageObject) => {
+  const { username, socketId, message, messageId, date } = messageObject;
+  const newMessage = {
+    username,
+    socketId,
+    message,
+    messageId,
+    date,
+  };
+  messages.push(newMessage);
+  console.log("Received message: ", newMessage);
+  io.emit("message", newMessage);
 });
 
 const PORT = process.env.PORT || 3000;
